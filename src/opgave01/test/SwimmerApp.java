d6504784d22dec33ab64a65edbca4f1cd926e82e
Makkekk <karsten-kirkegaard@hotmail.com>
2024-11-03 16:40:28 +0100

    update

--- src/opgave01/test/SwimmerApp.java
+++ src/opgave01/test/SwimmerApp.java
@@ -2,10 +2,14 @@ package opgave01.test;
 
 import java.util.ArrayList;
 import opgave01.model.Swimmer;
+import opgave01.model.TrainingPlan;
 
 public class SwimmerApp {
-    
+
+
     public static void main(String[] args) {
+        TrainingPlan trainingPlanA = new TrainingPlan('B',10,6);
+        TrainingPlan trainingPlanB = new TrainingPlan('A',16,10);
 
         ArrayList<Double> lapTimes = new ArrayList<>();
         lapTimes.add(1.02);
@@ -15,7 +19,7 @@ public class SwimmerApp {
         lapTimes.add(1.02);
         lapTimes.add(1.04);
         lapTimes.add(0.99);
-        Swimmer swimmer1 = new Swimmer("Jan", 1994, lapTimes, "AGF");
+        Swimmer swimmer1 = new Swimmer("Jan", 1994, lapTimes, "AGF", trainingPlanA);
 
         lapTimes = new ArrayList<>();
         lapTimes.add(1.05);
@@ -25,7 +29,7 @@ public class SwimmerApp {
         lapTimes.add(1.08);
         lapTimes.add(1.04);
         lapTimes.add(1.02);
-        Swimmer swimmer2 = new Swimmer("Bo", 1995, lapTimes, "Lyseng");
+        Swimmer swimmer2 = new Swimmer("Bo", 1995, lapTimes, "Lyseng",trainingPlanA);
 
         lapTimes = new ArrayList<>();
         lapTimes.add(1.03);
@@ -35,7 +39,7 @@ public class SwimmerApp {
         lapTimes.add(1.03);
         lapTimes.add(1.06);
         lapTimes.add(1.03);
-        Swimmer swimmer3 = new Swimmer("Mikkel", 1993, lapTimes, "AIA-Tranbjerg");
+        Swimmer swimmer3 = new Swimmer("Mikkel", 1993, lapTimes, "AIA-Tranbjerg",trainingPlanB);
         
         ArrayList<Swimmer> swimmers = new ArrayList<>();
         swimmers.add(swimmer1);
@@ -46,6 +50,7 @@ public class SwimmerApp {
             System.out.println(swimmer.getName() + "'s bedste tid: " + swimmer.bestLapTime());
         }
 
+
+
+        }
     }
-    
-}
